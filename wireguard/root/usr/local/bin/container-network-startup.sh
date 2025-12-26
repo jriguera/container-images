f282ea8d50d6a16270eb6ab83c7662c01355a370
Jose Riguera <jriguera@gmail.com>
2025-12-26 21:52:08 +0100

    fix scripts

--- wireguard/root/usr/local/bin/container-network-startup.sh
+++ wireguard/root/usr/local/bin/container-network-startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+set -x
 
 # Disable reverse path filtering (required for asymmetric routing)
-sysctl -w net.ipv4.conf.wg0.rp_filter=0
 sysctl -w net.ipv4.conf.eth1.rp_filter=0
 
 # Allow forwarding between internal network and WireGuard
@@ -13,5 +13,3 @@ iptables -t nat -I POSTROUTING 1 -s ${INTERNAL_NET_SUBNET} -o wg0 -j MASQUERADE
 # Policy routing: marked packets (fwmark 2) use table 200 (provider gateway)
 ip route add default via ${PROVIDER_NET_GW} table 200
 ip rule add fwmark ${IPTABLES_MANGLE_MARK_PUBLISHED_PORTS} table 200
-
-echo "startup done"
